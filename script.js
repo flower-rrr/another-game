af386b67b7341a767215179ed2726ee6971b670a
flower-rrr <123u123u123i123o1h2ugf@gmail.com>
2026-09-10 16:46:31 +0800

    Initialize game loop and input handling

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const canvas = document.getElementById("game")
 
 const ctx = canvas.getContext("2d")
+
+window.addEventListener('keydown', (e) => keys[e.key] = true);
+window.addEventListener('keyup', (e) => keys[e.key] = false);
+
+function update() {
+
+
+}
+
+function draw() {
+
+
+}
+
+function gameloop() {
+  update()
+  draw()
+  requestAnimationFrame(gameloop)
+}
+
