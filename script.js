d8e6bf495d033b4b0e624e31ac8f57a21399d5c5
flower-rrr <123u123u123i123o1h2ugf@gmail.com>
2026-09-12 14:07:40 +0800

    Refactor game loop and add title drawing function

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const canvas = document.getElementById("game")
+const canvas = document.getElementById("game");
 
-const ctx = canvas.getContext("2d")
+const ctx = canvas.getContext("2d");
+let ui = "title";
+
+const keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowLeft: false, 
+  ArrowRight: false
+};
 
 window.addEventListener('keydown', (e) => keys[e.key] = true);
 window.addEventListener('keyup', (e) => keys[e.key] = false);
+
 
 function gameUpdate() {
 
@@ -15,9 +24,17 @@ function gameDraw() {
 
 }
 
-function gameLoop() {
-  gameUpdate()
-  gameDraw()
-  requestAnimationFrame(gameloop)
+function titleDraw() {
+
+}
+
+function mainLoop() {
+  if (ui == "game") {
+    gameUpdate();
+    gameDraw();
+  } else if (ui == "title") {
+    titleDraw();
+  }
+  requestAnimationFrame(mainloop);
 }
 
