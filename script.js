33903cebbde79b5fb0fdf5f6bfa8b8178f3f982b
flower-rrr <123u123u123i123o1h2ugf@gmail.com>
2026-09-16 15:08:03 +0800

    Expose startGame function to the global scope

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const keys = {
 window.addEventListener('keydown', (e) => keys[e.key] = true);
 window.addEventListener('keyup', (e) => keys[e.key] = false);
 
-const startGame = function() {ui = "game"};
+window.startGame = function() {ui = "game"};
 
 function gameUpdate() {
 
